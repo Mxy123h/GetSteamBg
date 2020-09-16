3fce19003cb0b099bf65451a44fbd13cc662f0cc
XY M <mxy123h@gmail.com>
2020-09-16 17:53:49 +0800

    添加异常 设置代理

---
+++ GetSteamBg/MyException.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GetSteamBg
+{
+    /// <summary>
+    /// 自定义异常
+    /// </summary>
+    public class MyException : ApplicationException
+    {
+        public MyException(string errorcode, string message, object list) : base(message)
+        {
+            Errorcode = errorcode;
+            List = list;
+        }
+
+        public MyException(string myMessage, string message) : base(message)
+        {
+            MyMessage = myMessage;
+            message = message;
+        }
+
+        public override string Message
+        {
+            get
+            {
+                return base.Message;
+            }
+        }
+
+        public string Errorcode { get; }
+        public string MyMessage { get; }
+        public object List { get; }
+    }
+
+    public class HISOrderCancleException : ApplicationException
+    {
+        public HISOrderCancleException(string errorcode, string message) : base(message)
+        {
+            Errorcode = errorcode;
+        }
+
+        public override string Message
+        {
+            get
+            {
+                return base.Message;
+            }
+        }
+
+        public string Errorcode { get; }
+    }
+}
--- GetSteamBg/Program.cs
+++ GetSteamBg/Program.cs
@@ -13,24 +13,49 @@ namespace GetSteamBg
 {
     class Program
     {
+        static WebClient webClient = new WebClient();
+        static WebProxy proxyObject = null;
         static void Main(string[] args)
         {
-            Console.WriteLine("PageNo: 按回车默认为0");
-            string inputPageNo= Console.ReadLine();
-            inputPageNo = inputPageNo.Equals("") ? "0" : inputPageNo;
-            Console.WriteLine($"正在从第{inputPageNo}页开始抓取");
-            for (int i= int.Parse(inputPageNo); i<2999;i++) {
-                int page = i*100;
-                string url = @"https://steamcommunity.com/market/search/render/?query=&start=" + page.ToString() + @"&count=100&search_descriptions=0&sort_column=name&sort_dir=asc&appid=753&category_753_Game%5B%5D=any&category_753_item_class%5B%5D=tag_item_class_3";
-                string retJson = _Get(url);
-                dynamic dynamic = JsonConvert.DeserializeObject(retJson);
-                string bgHtml = dynamic["results_html"].ToString();
-                string json=_GetBgHtml(bgHtml);
-                _Replace(i.ToString(),json);
-                Console.WriteLine($"第{i}页抓取完成");
-                Thread.Sleep(10000);
+            try
+            {
+                Console.WriteLine("PageNo: 按回车默认为0");
+                string inputPageNo = Console.ReadLine();
+                inputPageNo = inputPageNo.Equals("") ? "0" : inputPageNo;
+                Console.WriteLine("设置http代理 例:http://127.0.0.1:1080/  不设置直接按回车");
+                string httpProxyUrl = Console.ReadLine();
+                if (!httpProxyUrl.Equals(""))
+                {
+                    proxyObject = new WebProxy(httpProxyUrl, true);
+                    webClient.Proxy = proxyObject;
+                    Console.WriteLine($"设置http代理为{httpProxyUrl}");
+                }
+                Console.WriteLine($"正在从第{inputPageNo}页开始抓取");
+                for (int i = int.Parse(inputPageNo); i < 2999; i++)
+                {
+                    int page = i * 100;
+                    string url = @"https://steamcommunity.com/market/search/render/?query=&start=" + page.ToString() + @"&count=100&search_descriptions=0&sort_column=name&sort_dir=asc&appid=753&category_753_Game%5B%5D=any&category_753_item_class%5B%5D=tag_item_class_3";
+                    string retJson = _Get(url);
+                    dynamic dynamic = JsonConvert.DeserializeObject(retJson);
+                    string bgHtml = dynamic["results_html"].ToString();
+                    string json = _GetBgHtml(bgHtml);
+                    _Replace(i.ToString(), json);
+                    Console.WriteLine($"第{i}页抓取完成");
+                    Thread.Sleep(10000);
+                }
+            }
+            catch (MyException ex)
+            {
+
+                Console.WriteLine(ex.MyMessage+"   详细: "+ ex.Message);
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine(ex.Message);
             }
             
+
             while (true)
             {
             }
@@ -39,11 +64,15 @@ namespace GetSteamBg
         static string _Get(string url)
         {
             ServicePointManager.ServerCertificateValidationCallback = (sender, certificate, chain, sslPolicyErrors) => true;
-            var webClient = new WebClient();
-            WebProxy proxyObject = new WebProxy("http://127.0.0.1:7890/", true);//http代理 不需要请注释掉
-            webClient.Proxy = proxyObject;
-            string html = webClient.DownloadString(url);
-            return html;
+            try
+            {
+                string html = webClient.DownloadString(url);
+                return html;
+            }
+            catch (Exception ex)
+            {
+                throw new MyException("十有八九网不通",ex.Message);
+            }
         }
 
         private static string _GetBgHtml(string html)
@@ -69,7 +98,7 @@ namespace GetSteamBg
 
                         imgUrl = "is null";
                     }
-                    
+
                     Console.WriteLine(imgUrl);
                     BgInfo bgInfo = new BgInfo();
                     bgInfo.url = imgUrl;
@@ -89,13 +118,13 @@ namespace GetSteamBg
             public string url { get => url_; set => url_ = value; }
             public string marketurl { get => marketurl_; set => marketurl_ = value; }
         }
-        static void _Replace(string pageNo,string json)
+        static void _Replace(string pageNo, string json)
         {
             StreamReader sr = new StreamReader("html/mb.html");
-            string temp_mb= sr.ReadToEnd();
+            string temp_mb = sr.ReadToEnd();
             sr.Close();
-            temp_mb=temp_mb.Replace("@@@@@@@@@@",json);
-            StreamWriter sw = new StreamWriter("html/"+pageNo + ".html");
+            temp_mb = temp_mb.Replace("@@@@@@@@@@", json);
+            StreamWriter sw = new StreamWriter("html/" + pageNo + ".html");
             sw.Write(temp_mb);
             sw.Close();
         }
